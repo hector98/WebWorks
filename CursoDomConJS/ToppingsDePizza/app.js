1a134849a01a3692138af89d80031b865c95160f
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2023-12-19 22:22:30 -0600

    Terminar proyecto de introduccion toppings de pizza

--- CursoDomConJS/ToppingsDePizza/app.js
+++ CursoDomConJS/ToppingsDePizza/app.js
@@ -1,12 +1,16 @@
-const container = document.getElementById('container');
-console.log(container);
-
-const toppings = document.getElementsByClassName('topping');
-console.log(toppings);
-
-const primerTopping = document.querySelector('.topping');
+//const container = document.getElementById('container');
+//console.log(container);
+//const toppings = document.getElementsByClassName('topping');
+//console.log(toppings);
+//const primerTopping = document.querySelector('.topping');
 //primerTopping.style.backgroundColor = 'blue';
 
-function mostrarClick(topping) {
-		console.log(topping);
+const toppings = document.getElementsByClassName('topping');
+function mostrarClick(e) {
+		console.log(e.target.innerText);
+}
+
+for (const topping of toppings)
+{
+		topping.addEventListener('click', mostrarClick);
 }
