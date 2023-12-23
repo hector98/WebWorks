0de9a0157de93b3aaf5a9679d99cbd7ae1087503
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2023-12-23 00:40:09 -0600

    Proyecto Citas aleatorias terminado, solo falta agragar mas citas

---
+++ CursoDomConJS/CitasAleatorias/citas.js
@@ -0,0 +1,34 @@
+let citas = [
+				{ 
+								'autor': 'Steve Wozniak',
+								'texto': 'Los artistas trabajan mejor solos. Trabaja solo.'
+				},
+				{
+								'autor': 'Elon Musk',
+								'texto': 'La persistencia es muy importante. No debes renunciar al menos que te veas obligado a renunciar.'
+				},
+        {
+								'autor': 'Nikola Tesla',
+								'texto': 'Si quieres encontrar los secretos del universo, piensa en términos de energía, frecuencia y vibración.'
+				},
+				{
+								'autor': 'Neli Armstrong',
+								'texto': 'Es un pequeño paso para un hombre, pero un salto gigante para la humanidad.'
+				},
+				{
+								'autor': 'Leonardo da Vinci',
+								'texto': 'El placer más noble es el júbilo de comprender.'
+				},
+				{
+								'autor': 'Enzo Ferrari',
+								'texto': 'La aerodinámica es para fracasados que no saben hacer motores.'
+				},
+				{
+								'autor': 'Elbert Hubbard',
+								'texto': 'Una máquina puede hacer el trabajo de 50 hombres ordinarios, pero ninguna máquina puede hacer el trabajo de un hombre extraordinario'
+				},
+				{
+								'autor': 'Steve Wozniak',
+								'texto': 'Nunca te fíes de un ordenador que no puedas tirar por la ventana.'
+				}
+];
